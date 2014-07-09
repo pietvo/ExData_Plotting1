3138ca0cd2c0198b0a2be980dcedd14489cde820
Piet van Oostrum <piet@vanoostrum.org>
2014-07-09 15:50:26 +0200

    Added plot2 files

---
+++ plot2.R
@@ -0,0 +1,19 @@
+# Read in the data
+
+hh <- read.csv("household_power_consumption.txt", sep=";", 
+               na.strings="?", stringsAsFactors=FALSE)
+
+# Subset for dates 2007-02-01 and 2007-02-02
+
+hh1 <- subset(hh, Date=="1/2/2007" | Date=="2/2/2007")
+
+# Convert Date/Time to POSIXct
+
+hh1$Time <- as.POSIXct(strptime(paste(hh1$Date, hh1$Time), "%d/%m/%Y %H:%M:%S"))
+
+# Plot a PNG file
+
+png(filename = "plot2.png", width = 480, height = 480)
+plot(hh1$Time, hh1$Global_active_power, xlab="", 
+     ylab="Global Active Power (kiloWatts)", type="l")
+dev.off()
