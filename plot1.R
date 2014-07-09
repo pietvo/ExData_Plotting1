27bef273510b0383b7a3a4f4f4a7a5924a093b50
Piet van Oostrum <piet@vanoostrum.org>
2014-07-09 15:28:47 +0200

    Added plot1 files

---
+++ plot1.R
@@ -0,0 +1,15 @@
+# Read in the data
+
+hh <- read.csv("household_power_consumption.txt", sep=";", 
+               na.strings="?", stringsAsFactors=FALSE)
+
+# Subset for dates 2007-02-01 and 2007-02-02
+
+hh1 <- subset(hh, Date=="1/2/2007" | Date=="2/2/2007")
+
+# Plot a PNG file
+
+png(filename = "plot1.png", width = 480, height = 480)
+hist(hh1$Global_active_power, col="red", main="Global Active Power",
+     xlab="Global Active Power (kiloWatts)")
+dev.off()
